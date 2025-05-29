2d651295d9f9ff56315c571358b6dde29f06a5e1
eugeniomorocho <sf4e@outlook.com>
2025-05-28 20:14:46 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,7 +3,7 @@ import joblib
 # Datos fijos para prueba
 features = [[5.1, 3.5, 1.4, 0.2]]
 # Cargar el modelo
-modelo = joblib.load("modelo.pkl")
+modelo = joblib.load("knn_model.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
