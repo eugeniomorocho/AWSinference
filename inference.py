af94bd668fb84e3e649bba08f9a39fea5a6f535b
eugeniomorocho <sf4e@outlook.com>
2025-05-28 20:09:52 -0500

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,9 @@
+import joblib
+
+# Datos fijos para prueba
+features = [[5.1, 3.5, 1.4, 0.2]]
+# Cargar el modelo
+modelo = joblib.load("modelo.pkl")
+# Hacer la predicción
+prediccion = modelo.predict(features)
+print("Predicción:", prediccion[0])
